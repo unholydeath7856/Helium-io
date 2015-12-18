33b2ddf86f90b6c16920cd6e6e5851ce2fc51eb7
Evan Coulson <evancoulson@iMac.local>
2015-12-18 11:33:57 -0800

    more file creation basic html structure added to index

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+  
+?>
 <!DOCTYPE html>
 <html>
   <head>
@@ -5,6 +8,27 @@
     <title>Helium.io</title>
   </head>
   <body>
+    <header class="header">
 
+    </header>
+
+    <section id="about" class="about">
+
+    </section>
+
+    <section id="map" class="map">
+      <div class="submit">
+        <form class="map-submit" action="index.php" method="post">
+
+        </form>
+      </div>
+      <div id="" class="map">
+
+      </div>
+    </section>
+
+    <footer>
+
+    </footer>
   </body>
 </html>
