0587ffda793a6d801eace3a90c73c61486fa7e1e
Evan Coulson <evancoulson@iMac.local>
2015-12-20 10:05:46 -0800

    Navbar in basic form, as well as a test selct elem in index

--- index.php
+++ index.php
@@ -1,14 +1,31 @@
 <?php
-  
+
 ?>
 <!DOCTYPE html>
 <html>
   <head>
     <meta charset="utf-8">
     <title>Helium.io</title>
+    <link rel="stylesheet" href="Assets/Styles/CSS/main.css" media="screen" title="no title" charset="utf-8">
   </head>
   <body>
-    <header class="header">
+
+    <div class="navbar-pop">
+      <div class="navbar-container">
+        <div class="nav-button">
+          
+        </div>
+        <ul class="nav-list">
+          <li class="nav-item"><a class="nav-item-link" href="#top">Top</a></li>
+          <li class="nav-item"><a class="nav-item-link" href="#about">About</a></li>
+          <li class="nav-item"><a class="nav-item-link" href="#submit">Submit</a></li>
+          <li class="nav-item"><a class="nav-item-link" href="#map">Map</a></li>
+          <li class="nav-item"><a class="nav-item-link" href="#bot">Bottom</a></li>
+        </ul>
+      </div>
+    </div>
+
+    <header id="top" class="header">
 
     </header>
 
@@ -19,7 +36,10 @@
     <section id="map" class="map">
       <div class="submit">
         <form class="map-submit" action="index.php" method="post">
-
+           <select class="state" name="state">
+             <option value="Al">Alabama</option>
+             <option value="Ak">Alaska</option>
+           </select>
         </form>
       </div>
       <div id="" class="map">
@@ -27,7 +47,7 @@
       </div>
     </section>
 
-    <footer>
+    <footer id="bot" class="footer">
 
     </footer>
   </body>
